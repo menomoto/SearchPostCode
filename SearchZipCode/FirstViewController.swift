e7b0b17bf3924835817022c9cf77883b7aef048d
menomoto <nob@nob.local>
2016-07-01 23:06:30 +0900

    add project initial files

---
+++ SearchZipCode/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SearchZipCode
+//
+//  Created by nob on 2016/07/01.
+//  Copyright © 2016年 menomoto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
