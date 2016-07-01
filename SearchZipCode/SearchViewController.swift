4bd5134bbca94ce590014ba79402c65e42a32338
menomoto <nob@nob.local>
2016-07-01 23:36:20 +0900

    検索タブ追加

---
+++ SearchZipCode/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  SearchZipCode
+//
+//  Created by nob on 2016/07/01.
+//  Copyright © 2016年 menomoto. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
