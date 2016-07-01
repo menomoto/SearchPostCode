7589771966e22de998cecdc63e8e0be1ac335a48
menomoto <nob@nob.local>
2016-07-02 00:59:49 +0900

    ダウンロード実装

--- SearchZipCode/HomeViewController.swift
+++ SearchZipCode/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  HomeViewController.swift
 //  SearchZipCode
 //
 //  Created by nob on 2016/07/01.
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+// debug kanagawa
+let zipCodeUrl = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/14kanaga.zip"
 
+class HomeViewController: UIViewController, NSURLSessionDownloadDelegate {
+
+    @IBAction func tapButton(sender: AnyObject) {
+        self.downloadFile()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,40 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func downloadFile() {
+        print("start Download")
+        // 通信のコンフィグを用意
+        let myConfig:NSURLSessionConfiguration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier("backgroundSession")
+        
+        // Sessionを作成する
+        let mySession:NSURLSession = NSURLSession(configuration: myConfig, delegate: self, delegateQueue: nil)
+        
+        // ダウンロード先のURLからリクエストを生成
+        if let myURL:NSURL = NSURL(string: zipCodeUrl) {
+            let myRequest:NSURLRequest = NSURLRequest(URL: myURL)
+            // ダウンロードタスクを生成
+            let myTask:NSURLSessionDownloadTask = mySession.downloadTaskWithRequest(myRequest)
+            
+            // タスクを実行
+            myTask.resume()
+        }
+        print("finish Download")
 
+    }
+    
+    //ダウンロード終了時に呼び出されるデリゲート
+    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
+    }
+    
+    //タスク終了時に呼び出されるデリゲート
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        
+        if error == nil {
+            print("ダウンロードが完了しました")
+        } else {
+            print("ダウンロードが失敗しました")
+        }
+        
+    }
 }
 
